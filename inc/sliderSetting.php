99eee4c51fd209e7a5892fcded575d89dd32842b
paster0 <paster01@gmail.com>
2020-04-18 15:54:37 +0200

    no message

--- inc/sliderSetting.php
+++ inc/sliderSetting.php
@@ -5,7 +5,18 @@
 
 <script>
 tinymce.init({
-    selector: '.sliderContent'
+    selector: '.sliderContent',
+    height: 500,
+  menubar: 'tools',
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table paste code help wordcount'
+  ],
+  toolbar: 'undo redo | formatselect | ' +
+  'bold italic backcolor | alignleft aligncenter ' +
+  'alignright alignjustify | bullist numlist outdent indent | ' +
+  'removeformat | help', 
 });
 </script>
 
