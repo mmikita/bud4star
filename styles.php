7b4ee16891b2b28dab753962e7703722d39b9501
paster0 <paster01@gmail.com>
2020-04-18 20:31:11 +0200

    no message

--- styles.php
+++ styles.php
@@ -16,11 +16,11 @@ img.mLogo{
     <?php if( get_theme_mod( 'primaryMenuUpper' ) == true) { ?> text-transform: uppercase; <?php }?>
 }
 <?php if( get_theme_mod( 'bootstrapMenuFont' ) == true) { ?> 
-    <?php if( get_theme_mod( 'xs-menufont' ) != 0 || get_theme_mod( 'xs-menufont' ) != '') { ?> .body-xs #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('xs-menufont').'px' ?>;} <?php }?>
-    <?php if( get_theme_mod( 'sm-menufont' ) != 0 || get_theme_mod( 'sm-menufont' ) != '') { ?> .body-sm #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('sm-menufont').'px' ?>;} <?php }?>
-    <?php if( get_theme_mod( 'md-menufont' ) != 0 || get_theme_mod( 'md-menufont' ) != '') { ?> .body-md #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('md-menufont').'px' ?>;} <?php }?>
-    <?php if( get_theme_mod( 'lg-menufont' ) != 0 || get_theme_mod( 'lg-menufont' ) != '') { ?> .body-lg #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('lg-menufont').'px' ?>;} <?php }?>
-    <?php if( get_theme_mod( 'xl-menufont' ) != 0 || get_theme_mod( 'xl-menufont' ) != '') { ?> .body-xl #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('xl-menufont').'px' ?>;} <?php }?>
+    <?php if( get_theme_mod( 'xs-menufont' ) != '0' && get_theme_mod( 'xs-menufont' ) != '') { ?> .body-xs #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('xs-menufont').'px' ?>;} <?php }?>
+    <?php if( get_theme_mod( 'sm-menufont' ) != '0' && get_theme_mod( 'sm-menufont' ) != '') { ?> .body-sm #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('sm-menufont').'px' ?>;} <?php }?>
+    <?php if( get_theme_mod( 'md-menufont' ) != '0' && get_theme_mod( 'md-menufont' ) != '') { ?> .body-md #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('md-menufont').'px' ?>;} <?php }?>
+    <?php if( get_theme_mod( 'lg-menufont' ) != '0' && get_theme_mod( 'lg-menufont' ) != '') { ?> .body-lg #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('lg-menufont').'px' ?>;} <?php }?>
+    <?php if( get_theme_mod( 'xl-menufont' ) != '0' && get_theme_mod( 'xl-menufont' ) != '') { ?> .body-xl #primaryMenu .nav-link{ font-size: <?php echo get_theme_mod('xl-menufont').'px' ?>;} <?php }?>
         <?php }?>
 
 #primaryMenu li.active a {-webkit-box-shadow: inset 0px 7px 0px 0px <?php echo get_theme_mod('menuHoverColor', '#0a7db8')?>; color: <?php echo get_theme_mod('menuHoverColor', '#0a7db8')?>}
@@ -30,24 +30,24 @@ img.mLogo{
 
 <?php if( get_theme_mod( 'spaceBetweenPrimaryBoot' ) == true) { ?> 
     
-    <?php if( get_theme_mod( 'xs-spaceBetweenPrimary' ) != 0 || get_theme_mod( 'xs-spaceBetweenPrimary' ) != '' )
+    <?php if( get_theme_mod( 'xs-spaceBetweenPrimary' ) != 0 && get_theme_mod( 'xs-spaceBetweenPrimary' ) != '' )
      { ?> .body-xs #primaryMenu a{ padding-left: <?php echo get_theme_mod('xs-spaceBetweenPrimary').'px' ?>; 
         padding-right:<?php echo get_theme_mod('xs-spaceBetweenPrimary').'px' ?>; } <?php }?>
 
-        <?php if( get_theme_mod( 'sm-spaceBetweenPrimary' ) != 0 || get_theme_mod( 'sm-spaceBetweenPrimary' ) != '' ) 
+        <?php if( get_theme_mod( 'sm-spaceBetweenPrimary' ) != 0 && get_theme_mod( 'sm-spaceBetweenPrimary' ) != '' ) 
         { ?> .body-sm #primaryMenu a{ padding-left: <?php echo get_theme_mod('sm-spaceBetweenPrimary').'px' ?>; 
         padding-right:<?php echo get_theme_mod('sm-spaceBetweenPrimary').'px' ?>; } <?php }?>
 
         
-        <?php if( get_theme_mod( 'md-spaceBetweenPrimary' ) != 0 || get_theme_mod( 'md-spaceBetweenPrimary' ) != '' ) 
+        <?php if( get_theme_mod( 'md-spaceBetweenPrimary' ) != 0 && get_theme_mod( 'md-spaceBetweenPrimary' ) != '' ) 
         { ?> .body-md #primaryMenu a{ padding-left: <?php echo get_theme_mod('md-spaceBetweenPrimary').'px' ?>; 
         padding-right:<?php echo get_theme_mod('md-spaceBetweenPrimary').'px' ?>; } <?php }?>
 
-        <?php if( get_theme_mod( 'lg-spaceBetweenPrimary' ) != 0 || get_theme_mod( 'lg-spaceBetweenPrimary' ) != '' ) 
+        <?php if( get_theme_mod( 'lg-spaceBetweenPrimary' ) != 0 && get_theme_mod( 'lg-spaceBetweenPrimary' ) != '' ) 
         { ?> .body-lg #primaryMenu a{ padding-left: <?php echo get_theme_mod('lg-spaceBetweenPrimary').'px' ?>; 
         padding-right:<?php echo get_theme_mod('lg-spaceBetweenPrimary').'px' ?>; } <?php }?>
 
-        <?php if( get_theme_mod( 'xl-spaceBetweenPrimary' ) != 0 || get_theme_mod( 'xl-spaceBetweenPrimary' ) != '' ) 
+        <?php if( get_theme_mod( 'xl-spaceBetweenPrimary' ) != 0 && get_theme_mod( 'xl-spaceBetweenPrimary' ) != '' ) 
         { ?> .body-xl #primaryMenu a{ padding-left: <?php echo get_theme_mod('xl-spaceBetweenPrimary').'px' ?>; 
         padding-right:<?php echo get_theme_mod('xl-spaceBetweenPrimary').'px' ?>; } <?php }?>
 
